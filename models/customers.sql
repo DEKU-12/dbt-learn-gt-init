a94f853fb060c16c1ba9145f99b63d8192b53791
tealiie le <tealiie.le@dbtlabsworkshop.com>
2026-02-24 06:01:02 +0000

    Build dim_customers and rename cols in staging customers and orders rather than in the dim file

--- models/customers.sql
+++ models/customers.sql
@@ -7,30 +7,40 @@
 with
     customers as (
 
-        select id as customer_id, first_name, last_name from raw.jaffle_shop.customers
+        select 
+            id  
+            , first_name
+            , last_name 
+
+        --from raw.jaffle_shop.customers
+        from {{ ref('stg_jaffle_shop__customers') }}
 
     ),
 
     orders as (
 
-        select id as order_id, user_id as customer_id, order_date, status
+        select 
+            order_id    --id as order_id, these get renamed in the staging layer
+            , customer_id   --, user_id as customer_id
+            , order_date
+            , status
 
-        from raw.jaffle_shop.orders
+        --from raw.jaffle_shop.orders
+        from {{ ref('stg_jaffle_shop__orders') }}
 
     ),
 
     customer_orders as (
 
         select
-            customer_id,
-
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
+            customer_id
+            , min(order_date) as first_order_date
+            , max(order_date) as most_recent_order_date
+            , count(order_id) as number_of_orders
 
         from orders
 
-        group by 1
+        group by customer_id
 
     ),
 
